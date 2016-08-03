54be0ed21b621f4069b32c51796f09fed2bbcd05
scienceswork <2227533225@qq.com>
2016-08-03 11:34:39 +0800

    校园o2o店家后台项目框架初始化

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name adminWebFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adminWebFrontApp
+ */
+angular.module('adminWebFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
